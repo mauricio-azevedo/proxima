961b852459c093558ef1a7a19b7b03ce54ab213e
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-01 18:50:36 -0300

    feat(api): enable cors and bind host

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
-bootstrap();
+
+void bootstrap();
