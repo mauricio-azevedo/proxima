2d2f64ae3f7e71cd9c935be29102d4b8a38b1713
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-01 18:49:50 -0300

    feat(api): add auth controller

---
+++ api/src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { AuthenticatedRequest } from './authenticated-request';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() body: RegisterDto) {
+    return this.authService.register(body);
+  }
+
+  @Post('login')
+  login(@Body() body: LoginDto) {
+    return this.authService.login(body);
+  }
+
+  @Get('me')
+  @UseGuards(AuthGuard)
+  me(@Req() request: AuthenticatedRequest) {
+    return this.authService.me(request.user.id);
+  }
+}
