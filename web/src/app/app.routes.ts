ec3a1b9a28370ff076a724006812db79b570ace5
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-02 00:37:24 -0300

    feat(web): add auth routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { routeAuthGuard } from './core/auth/route-auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login.page').then((module) => module.LoginPage),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register.page').then(
+        (module) => module.RegisterPage,
+      ),
+  },
+  {
+    path: 'app',
+    canActivate: [routeAuthGuard],
+    loadComponent: () =>
+      import('./features/protected-home/protected-home.page').then(
+        (module) => module.ProtectedHomePage,
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'app',
+  },
+  {
+    path: '**',
+    redirectTo: 'app',
+  },
+];
