d5176d388a2b45b5c3962cb1383c96b33a496f2f
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-03 21:22:53 -0300

    Register game days service

--- api/src/pickup-games/pickup-games.module.ts
+++ api/src/pickup-games/pickup-games.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../database/prisma.module';
+import { GameDaysService } from './game-days.service';
 import { PickupGamesController } from './pickup-games.controller';
 import { PickupGamesService } from './pickup-games.service';
 
 @Module({
   imports: [AuthModule, PrismaModule],
   controllers: [PickupGamesController],
-  providers: [PickupGamesService],
+  providers: [PickupGamesService, GameDaysService],
 })
 export class PickupGamesModule {}
