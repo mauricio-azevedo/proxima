67e2946fc0c95b37adde0285bfbb9ed851fd7190
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-04 20:01:44 -0300

    Add default development user session

---
+++ web/src/features/auth/constants/default-user-session.ts
@@ -0,0 +1,6 @@
+import type { UserSession } from '../../../app/types/user-session';
+
+export const defaultUserSession: UserSession = {
+  name: 'Maurício Azevedo',
+  email: 'mauricio@example.com',
+};
