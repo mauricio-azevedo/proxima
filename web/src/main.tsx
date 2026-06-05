b1a8135142337ec70cf9f87e21b51cd5192d960c
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-04 21:05:44 -0300

    Provide persisted color theme

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './app/App';
+import './index.css';
+import { ColorThemeProvider } from './shared/theme/provider/ColorThemeProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ColorThemeProvider>
+      <App />
+    </ColorThemeProvider>
   </StrictMode>,
-)
+);
