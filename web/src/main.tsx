79a187aa56b432479d2404b1bdeefb4b3f985570
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-05 00:21:15 -0300

    Wrap app with browser router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 
 import { App } from './app/App';
 import './index.css';
@@ -11,8 +12,10 @@ applyColorTheme(getStoredColorTheme());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ColorThemeProvider>
-      <App />
-    </ColorThemeProvider>
+    <BrowserRouter>
+      <ColorThemeProvider>
+        <App />
+      </ColorThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
