1900c52de099b336213ee30f0cf9b42469c30085
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-04 21:53:06 -0300

    Remove mock default user session

--- web/src/features/auth/constants/default-user-session.ts
+++
@@ -1,6 +0,0 @@
-import type { UserSession } from '../../../app/types/user-session';
-
-export const defaultUserSession: UserSession = {
-  name: 'Maurício Azevedo',
-  email: 'mauricio@example.com',
-};
