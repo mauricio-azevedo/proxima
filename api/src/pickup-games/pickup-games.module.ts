a31bd58297c037c4ed954f79fe1319d5241f7794
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-07 21:15:28 -0300

    Register pickup games home service

--- api/src/pickup-games/pickup-games.module.ts
+++ api/src/pickup-games/pickup-games.module.ts
@@ -4,11 +4,12 @@ import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../database/prisma.module';
 import { GameDaysService } from './game-days.service';
 import { PickupGamesController } from './pickup-games.controller';
+import { PickupGamesHomeService } from './pickup-games-home.service';
 import { PickupGamesService } from './pickup-games.service';
 
 @Module({
   imports: [AuthModule, PrismaModule],
   controllers: [PickupGamesController],
-  providers: [PickupGamesService, GameDaysService],
+  providers: [PickupGamesService, PickupGamesHomeService, GameDaysService],
 })
 export class PickupGamesModule {}
